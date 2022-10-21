716cd51bf475ebd56aa6920634729dfec315b77d
Maksim Veprev <maxveprevspb@gmail.com>
2022-10-21 16:49:30 +0100

    Add method isPrime to check if a number is prime, modify primes method to return the list of first n prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
+def isPrime(n):
+    for i in range(2, n):
+        if(n%i==0):
+            return False
+    return True
 
 def primes(number_of_primes):
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        if(isPrime(i)):
+            list.append(i)
+        i += 1
     return list
